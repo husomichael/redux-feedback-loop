6b0c1dc8f40b1339647e4b77adaacd3a310a5959
Michael Huso <husomichael@gmail.com>
2021-12-04 18:16:42 -0600

    App header moved to Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header(){
+    return(
+        <div background-color="blue">
+            <h1>Feedback!</h1>
+            <h4>Don't forget it!</h4>
+        </div>
+    );
+};
+
+export default Header;
