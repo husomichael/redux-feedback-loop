cf86693a108c1d5aac25fb42d430959c0e226f0d
Michael Huso <husomichael@gmail.com>
2021-12-04 18:02:37 -0600

    feedbackReducer switch statement setup with 5 cases

--- src/index.js
+++ src/index.js
@@ -9,8 +9,18 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 
 const feedbackReducer = (state = {}, action) => {
     switch(action.type) {
-        //4 feedback cases here,
-        //1 clear case for DB post route
+        case "ADD_FEELING":
+            return state.feeling = action.payload;
+        case "ADD_UNDERSTANDING":
+            return state.understanding = action.payload;
+        case "ADD_SUPPORT":
+            return state.support = action.payload;
+        case "ADD_COMMENT":
+            return state.comment = action.payload;
+        case "CLEAR_FEEDBACK":
+            return {};
+        default:
+            return {};
     };
 };
 
